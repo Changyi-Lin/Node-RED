378b61e0775a5882c298c3f7bc71c1bf57ee2d7f
Changyi-Lin <a0984360579@gmail.com>
2021-09-27 14:12:56 +0800

    修改switch的Bug 修改其他檔案:json to js

--- switch.js
+++ switch.js
@@ -32,53 +32,44 @@ function check()
         function(res)
         {
             window.inf=res.Inf;
-            var length = window.inf.length; 
+            window.inf_length = window.inf.length; 
             
             var Tdate=new Date();
             window.Tyear=Tdate.getFullYear();
             window.Tmonth=Tdate.getMonth();
             window.Tday=Tdate.getDate();
             window.currrent_h=Tdate.getHours();
+            
+            var hour_index = ["A","B","C","D","F","G","H","I"];
+            
             if(window.currrent_h>=8 && window.currrent_h<=17)
             {
                 if(window.currrent_h<12 && window.currrent_h>=8)
                 {
                     var j = window.currrent_h-8;
                 }
-                if(window.currrent_h>12 && window.currrent_h<=17)
+                if(window.currrent_h>=13 && window.currrent_h<18)
                 {
                     var j = window.currrent_h-13+4;
                 }
-                var hour_index = ["A","B","C","D","F","G","H","I"];
-                var current_index = hour_index[j];
+                window.current_index = hour_index[j];
             }
 
-            for(i=0;i<length;i++)
+            for(i=0;i<window.inf_length;i++)
             {
-                var date_index = new Date(window.inf[i].Time);
-                if( date_index.getFullYear() == window.Tyear)
+                window.date_index = new Date(window.inf[i].Time);
+                var index = window.inf[i].lend.split("");
+                var index_length = index.length;
+                for(k=0;k<index_length;k++)
                 {
-                    if( date_index.getMonth() == window.Tmonth)
+                    if(index[k] == window.current_index)
                     {
-                        if( date_index.getDate() == window.Tday)
-                        {
-                            var index = window.inf[i].lend.split("");
-                            var index_length = index.length;
-                            console.log(index);
-                            for(i=0;i<index_length;i++)
-                            {
-                                if(index[i] == current_index)
-                                {
-                                    window.check_flag = 1;
-                                }
-                            }
-                            
-                        }    
+                        window.check_flag = 1;
                     }
                 }
             }
 
-            if(window.check_flag!=1)
+            if(window.check_flag==0)
             {
                 switch_off();
                 window.alert("目前非您的借閱時間");

b08a67286ac6b24388263261e46ce6b0ca7eb445
Changyi-Lin <a0984360579@gmail.com>
2021-12-21 18:41:53 +0800

    完成98%，剩下硬體連接測試。

--- switch.js
+++ switch.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
+        window.Today = new Date(); // 今天的日期    
         check();
 
         $("#switchon").click(function() {
-            check_uid();
+            check_cookie();
             check();
             switch_on();
         })
 
         $("#switchoff").click(function() {
-            check_uid();
+            check_cookie();
             check();
             switch_off();
+        })
+
+        $("#backhome").click(function() {
+            window.location.href = "http://127.0.0.1:1880/web/home";
         })
     }
 
@@ -18,47 +23,28 @@ $(document).ready(function() {
 
 function check() {
     window.check_flag = 0;
+    var today = date_formate(window.Today);
     var API = "http://127.0.0.1:1880/check";
     $.post(
         API, {
-            Uid: window.uid
+            Today: today
         },
         function(res) {
-            window.inf = res.Inf;
-            window.inf_length = window.inf.length;
+            var inf = res.Inf;
+            var currrent_h = window.Today.getHours();
+            var j = 0;
 
-            var Tdate = new Date();
-            window.Tyear = Tdate.getFullYear();
-            window.Tmonth = Tdate.getMonth();
-            window.Tday = Tdate.getDate();
-            window.currrent_h = Tdate.getHours();
-
-            var hour_index = ["A", "B", "C", "D", "F", "G", "H", "I"];
-
-            if (window.currrent_h >= 8 && window.currrent_h <= 17) {
-                if (window.currrent_h < 12 && window.currrent_h >= 8) {
-                    var j = window.currrent_h - 8;
+            if (currrent_h >= 8 && currrent_h <= 17) {
+                if (currrent_h < 12 && currrent_h >= 8) {
+                    j = currrent_h - 8;
                 }
-                if (window.currrent_h >= 13 && window.currrent_h < 18) {
-                    var j = window.currrent_h - 13 + 4;
-                }
-                window.current_index = hour_index[j];
-            }
-
-            for (i = 0; i < window.inf_length; i++) {
-                window.date_index = new Date(window.inf[i].Time);
-                var index = window.inf[i].lend.split("");
-                var index_length = index.length;
-                for (k = 0; k < index_length; k++) {
-                    if (index[k] == window.current_index) {
-                        window.check_flag = 1;
-                    }
+                if (currrent_h >= 13 && currrent_h < 18) {
+                    j = currrent_h - 13 + 4;
                 }
             }
 
-            if (window.check_flag == 0) {
-                switch_off();
-                window.alert("目前非您的借閱時間");
+            if (inf[j] != window.uid) {
+                alert("目前非您借用時間，將返回系統首頁。");
                 window.location.href = "http://127.0.0.1:1880/web/home";
             }
         }
@@ -85,9 +71,23 @@ function switch_off() {
     );
 }
 
-function check_uid() {
-    var API = "http://127.0.0.1:1880/web/check_uid"
-    $.get(
-        API
-    );
+function check_cookie() {
+    var API = "http://127.0.0.1:1880/web/check_cookie";
+    $.get(API);
+}
+
+function date_formate(date) {
+    date = new Date(date);
+    var date_year = date.getFullYear();
+    var date_month = date.getMonth() + 1;
+    var date_day = date.getDate();
+    if (date_day < 10) {
+        date_day = '0' + date_day;
+    }
+    if (date_month < 10) {
+        date_month = '0' + date_month;
+    }
+    var res = date_year + "-" + date_month + "-" + date_day;
+    return res;
+    // 僅格式化 年-月-日
 }
